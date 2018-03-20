f4768191e5ad658623f568d3cf3b0abae0867b1e
Matt Miller and Pablo Barreyro <matt@>
2018-03-20 09:37:48 -0700

    Add new elements under clickable2 class, apply slideToggle method to clickable2.

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
     $("#initially-hidden").toggle();
     $("#initially-showing").toggle();
   });
+
+  $(".clickable2").click(function() {
+    $("#initially-hidden2").slideToggle("slow");
+    // $("#initially-hidden2").fadeOut("slow");
+  });
+
 });
